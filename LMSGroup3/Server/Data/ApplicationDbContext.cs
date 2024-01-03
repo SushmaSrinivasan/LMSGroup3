40963141653f195705bf7179dfe2c5514e072466
marcusonline97 <Mjpersson97@gmail.com>
2024-01-03 16:30:12 +0100

    Revert "Merge branch 'Register-new-student-users' into New-Development"
    This reverts commit 4685f8f4bed3c1451eea7fe953166d3c6f5b97a0, reversing
    changes made to a8fb03160c322e11a73277bb26ea49a7692c44d9.

--- LMSGroup3/Server/Data/ApplicationDbContext.cs
+++ LMSGroup3/Server/Data/ApplicationDbContext.cs
@@ -13,11 +13,17 @@ namespace LMSGroup3.Server.Data
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
+
         public DbSet<Course> Courses { get; set; } = null!;
         public DbSet<Module> Modules { get; set; } = null!;
 
         public DbSet<Activity> Activities { get; set; } = null!;
 
-        public DbSet<ActivityType> ActivityTypes { get; set; } = null!;
+        public DbSet<ActivityType> ActivityTypes{ get; set; } = null!;
+
+
+
+
+
     }
 }
