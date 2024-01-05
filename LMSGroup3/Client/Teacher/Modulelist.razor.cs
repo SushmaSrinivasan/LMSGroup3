d0b839489a4692d13117bc9026145588e14cf4f1
Jossy <josukattoor@gmail.com>
2024-01-05 16:44:30 +0100

    errors resolved

--- LMSGroup3/Client/Teacher/Modulelist.razor.cs
+++ LMSGroup3/Client/Teacher/Modulelist.razor.cs
@@ -8,12 +8,12 @@ namespace LMSGroup3.Client.Teacher
     public partial class Moduleslist : ComponentBase
 
     {
-        private List<ModuleDto> modules;
-        private int courseid = 3;
-        protected override async Task OnInitializedAsync()
-        {
-            modules = await HttpClient.GetFromJsonAsync<List<ModuleDto>>($"api/Course/GetModulesByCourse?courseid={courseid}");
-        }
+        //private List<ModuleDto> modules;
+        //private int courseid = 3;
+        //protected override async Task OnInitializedAsync()
+        //{
+        //    modules = await HttpClient.GetFromJsonAsync<List<ModuleDto>>($"api/Course/GetModulesByCourse?courseid={courseid}");
+        //}
 
     }
 }
