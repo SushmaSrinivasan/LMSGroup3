7d9235bff1dbbae7a46cb7bc344a4e872386a2a0
marcusonline97 <Mjpersson97@gmail.com>
2024-01-05 10:48:10 +0100

    Worked on a CourseCard
    -The CourseCard is meant to contain the CourseInformation needed to identity which course is which.

---
+++ LMSGroup3/Client/Components/CourseCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using LMSGroup3.Shared;
+using LMSGroup3.Client;
+using LMSGroup3.Client.Teacher;
+namespace LMSGroup3.Client.Components
+{
+	public partial class CourseCard
+	{
+		public Courselist Courses { get; set; } = default!;
+	}
+}
