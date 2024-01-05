592dd8c0ad93fa9e62f231902e5f955f174fcb4a
marcusonline97 <Mjpersson97@gmail.com>
2024-01-05 04:51:17 +0100

    Update for start of the layout mockup

---
+++ LMSGroup3/Client/Pages/ManageCourse.razor.cs
@@ -0,0 +1,17 @@
+﻿using LMSGroup3.Client.Teacher;
+using LMSGroup3.Shared.Domain.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace LMSGroup3.Client.Pages
+{
+	public partial class ManageCourse : ComponentBase
+	{
+		public List<CourseDto> Courses { get; set; } = default!; //Need to implement the 
+													 //stored courses from the databases
+
+		protected override void OnInitialized()
+		{
+			//CourseDto = new CourseDto();  (I am unsure how to get the courses from the database
+		}
+	}
+}
