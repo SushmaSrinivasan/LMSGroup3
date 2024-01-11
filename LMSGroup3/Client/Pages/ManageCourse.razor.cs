5625250d815aa477ba8729a1ad1f2756e3bac985
Farah Alhaddad <120371923+Farahalh@users.noreply.github.com>
2024-01-11 16:53:58 +0100

    Re-creating the code-behind for MAnageCourses

---
+++ LMSGroup3/Client/Pages/ManageCourse.razor.cs
@@ -0,0 +1,24 @@
+﻿using LMSGroup3.Client.Teacher;
+using LMSGroup3.Shared.Domain.DTOs;
+using LMSGroup3.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace LMSGroup3.Client.Pages
+{
+    public partial class ManageCourse
+    {
+        public List<CourseDto> Courses { get; set; } = default!; //Need to implement the 
+                                                                 //stored courses from the databases
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCourses();
+        }
+
+        private async Task LoadCourses()
+        {
+            Courses = await HttpClient.GetFromJsonAsync<List<CourseDto>>("api/Course/GetCourses");
+        }
+    }
+}
